acc4f12de4b7c4ceac3a682d91aa4cb4fae3c02e
Niveditha431 <nivedithajsayachandran431@gmail.com>
2025-09-10 12:25:05 +1000

    edited script

--- MB5370_mod04.R
+++ MB5370_mod04.R
@@ -3,3 +3,20 @@ usethis::use_github()
 gitcreds::gitcreds_set()
 usethis::use_github()
 
+install.packages("tidyverse")
+library(tidyverse)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, colour = class))
+
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+
