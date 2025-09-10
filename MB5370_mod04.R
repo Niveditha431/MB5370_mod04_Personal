191fb3aef490dc2aaf4eae3cc6a28cca63ee144e
Niveditha431 <nivedithajsayachandran431@gmail.com>
2025-09-10 12:33:35 +1000

    saved

--- MB5370_mod04.R
+++ MB5370_mod04.R
@@ -20,3 +20,14 @@ ggplot(data = mpg) +
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy, shape = class))
 
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow = 2)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(drv ~ cyl)
+
+
